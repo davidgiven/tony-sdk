46b2d4603d7ae85d16cc7bafe74b43421a023e96
David Given <dg@cowlark.com>
2025-07-11 01:43:17 +0200

    Figure out the horrible ld overlay stuff.

--- Makefile
+++ Makefile
@@ -1,4 +1,12 @@
+export LLVM = /opt/pkg/llvm-mos/bin
+export CC6502 = $(LLVM)/mos-cpm65-clang
+export LD6502 = $(LLVM)/ld.lld
+export AR6502 = $(LLVM)/llvm-ar
+
 export OBJ = .obj
+
+export CFLAGS6502 = -Os -g
+export LDFLAGS6502 = -mlto-zp=0
 
 .PHONY: all
 all: +all
--- build.py
+++ build.py
@@ -1,5 +1,24 @@
 from build.ab import export
+from build.llvm import llvmrawprogram, llvmclibrary
+
+llvmclibrary(
+    name="tony_lib",
+    hdrs={
+        "tony.inc": "./include/tony.inc"
+    })
+
+llvmrawprogram(
+    name="romimage",
+    srcs=["src/init.S", "src/main.S"],
+    deps=[".+tony_lib"],
+    ldflags=["--no-check-sections"],
+    linkscript="./tony.ld",
+)
+
 
 export(
     name="all",
+    items={
+        "tony.img": ".+romimage"
+    }
 )
